4422e3608dce0a170fe4f57489344e4e16e4fd57
inouesaya <40421889+inouesaya@users.noreply.github.com>
2018-06-20 19:13:51 +0900

    Update HelloWorld.java

--- myproject_hello/src/myproject_hello/HelloWorld.java
+++ myproject_hello/src/myproject_hello/HelloWorld.java
@@ -3,8 +3,9 @@ package myproject_hello;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO ©“®¶¬‚³‚ê‚½ƒƒ\ƒbƒhEƒXƒ^ƒu
+		// TODO è‡ªå‹•ç”Ÿæˆã•ã‚ŒãŸãƒ¡ã‚½ãƒƒãƒ‰ãƒ»ã‚¹ã‚¿ãƒ–
 System.out.println("HelloWorld!!!!");
+System.out.println("HelloGithub!!!");
 System.out.println("HelloGithub!!!");
 	}
 
